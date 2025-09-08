a0b99abdd6841341805801cfb9c61a9117f9c545
Haroldojo <156315453+Haroldojo@users.noreply.github.com>
2025-09-08 23:53:29 +0530

    Update main.py

--- Backend/main.py
+++ Backend/main.py
@@ -2,7 +2,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import uvicorn
-from aiagent import graph,SYSTEM_PROMPT,parse_response
+from .aiagent import graph,SYSTEM_PROMPT,parse_response
 
 
 app = FastAPI()
